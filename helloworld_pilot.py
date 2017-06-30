1f4d986440bd1040e9aeb5b66f0ba7b3e875a84d
Dereck Toker <dtoker@cs.ubc.ca>
2017-06-30 09:01:16 -0700

    a

--- helloworld_pilot.py
+++ helloworld_pilot.py
@@ -70,8 +70,8 @@ class MainHandler(tornado.web.RequestHandler):
         q1 = self.get_argument('element_1')
         if(int(q1)==1):
             self.application.mmd_order = [3,5,9,11,18,20,27,28,30,60,62,66,72,74,76] #removed MMD 73
-            self.application.mmd_order = [28,74,3] #removed MMD 73
-            #random.shuffle(self.application.mmd_order)
+            #self.application.mmd_order = [73] #removed MMD 73
+            random.shuffle(self.application.mmd_order)
             self.application.mmd_index = 0
 
             #self.redirect('/mmd')
