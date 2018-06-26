e8454e182072ab5b2785323e709f0fecce459ed2
grig-guz <prb578@icloud.com>
2018-06-26 15:17:46 -0700

    added params file

---
+++ params.py
@@ -0,0 +1,10 @@
+# Features to use
+USE_PUPIL_FEATURES = True
+USE_DISTANCE_FEATURES = True
+USE_PATH_FEATURES = True
+USE_FIXATION_FEATURES = True
+
+
+# Sets of features to keep
+KEEP_TASK_FEATURES = True
+KEEP_GLOBAL_FEATURES = True
